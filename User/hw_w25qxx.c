ff1af3c063fa8139f1be5841aa228538e8d49d93
chenshi <expresschs@163.com>
2023-07-30 20:51:54 +0800

    Update hw_w25qxx.c

--- User/hw_w25qxx.c
+++ User/hw_w25qxx.c
@@ -6,53 +6,39 @@
 #include "spi_drv.h"
 #include "hw_w25qxx.h"
 
-#define W25X_PageErase_Timeout      (3000UL)    //ms
-#define W25X_Operate_Timeout        (200UL)     //ms
-#define W25X_Dummy_Byte             (0xA5UL)
+#define W25X_PageErase_Timeout                  (3000UL)    //ms
+#define W25X_Operate_Timeout                    (200UL)     //ms
+#define W25X_Dummy_Byte                         (0xA5UL)
 
 //FLASH COMMAND
-#define W25X_WriteEnable            (0x06UL)
-#define W25X_WriteDisable           (0x04UL)
-#define W25X_ReadStatusReg1         (0x05UL)
-#define W25X_ReadStatusReg2         (0x35UL)
-#define W25X_ReadStatusReg3         (0x15UL)
-#define W25X_WriteStatusReg1        (0x01UL)
-#define W25X_WriteStatusReg2        (0x31UL)
-#define W25X_WriteStatusReg3        (0x11UL)
-#define W25X_ReadData               (0x03UL)
-#define W25X_FastReadData           (0x0BUL)
-#define W25X_FastReadDual           (0x3BUL)
-#define W25X_PageProgram            (0x02UL)
-#define W25X_BlockErase             (0xD8UL)
-#define W25X_SectorErase            (0x20UL)
-#define W25X_ChipErase              (0xC7UL)
-#define W25X_PowerDown              (0xB9UL)
-#define W25X_ReleasePowerDown       (0xABUL)
-#define W25X_DeviceID               (0xABUL)
-#define W25X_ManufactDeviceID       (0x90UL)
-#define W25X_JedecDeviceID          (0x9FUL)
-#define W25X_Enable4ByteAddr        (0xB7UL)
-#define W25X_Exit4ByteAddr          (0xE9UL)
+#define W25X_WriteEnable                        (0x06UL)
+#define W25X_WriteDisable                       (0x04UL)
+#define W25X_ReadStatusReg1                     (0x05UL)
+#define W25X_ReadStatusReg2                     (0x35UL)
+#define W25X_ReadStatusReg3                     (0x15UL)
+#define W25X_WriteStatusReg1                    (0x01UL)
+#define W25X_WriteStatusReg2                    (0x31UL)
+#define W25X_WriteStatusReg3                    (0x11UL)
+#define W25X_ReadData                           (0x03UL)
+#define W25X_FastReadData                       (0x0BUL)
+#define W25X_FastReadDual                       (0x3BUL)
+#define W25X_PageProgram                        (0x02UL)
+#define W25X_BlockErase                         (0xD8UL)
+#define W25X_SectorErase                        (0x20UL)
+#define W25X_ChipErase                          (0xC7UL)
+#define W25X_PowerDown                          (0xB9UL)
+#define W25X_ReleasePowerDown                   (0xABUL)
+#define W25X_DeviceID                           (0xABUL)
+#define W25X_ManufactDeviceID                   (0x90UL)
+#define W25X_JedecDeviceID                      (0x9FUL)
+#define W25X_Enable4ByteAddr                    (0xB7UL)
+#define W25X_Exit4ByteAddr                      (0xE9UL)
 
-static void w25qxx_delay(uint32_t ms)
-{
-    osDelay(ms);
-}
+#define w25qxx_delay(ms)                        osDelay(ms)
+#define w25qxx_spi_cs_low()                     spi_dev_cs(0, 0, 0)
+#define w25qxx_spi_cs_high()                    spi_dev_cs(0, 0, 1)
+#define w25qxx_spi_transfer(send, recv, size)   spi_dev_transfer(0, send, recv, size)
 
-static inline void w25qxx_spi_cs_low(void)
-{
-    spi_dev_cs(0, 0, 0);
-}
-
-static inline void w25qxx_spi_cs_high(void)
-{
-    spi_dev_cs(0, 0, 1);
-}
-
-static inline uint8_t w25qxx_spi_transfer(uint8_t *send, uint8_t *recv, uint32_t size)
-{
-    return spi_dev_transfer(0, send, recv, size);
-}
 
 static uint8_t w25qxx_wait_ready(uint32_t timeout)
 {
